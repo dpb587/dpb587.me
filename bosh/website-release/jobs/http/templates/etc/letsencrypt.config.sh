93455adada6a19c12ab77cc08f6c71bad6d92f0c
Danny Berger <dpb587@gmail.com>
2016-04-27 21:04:06 -0700

    CI task for renewing letsencrypt certificates

--- bosh/website-release/jobs/http/templates/etc/letsencrypt.config.sh
+++ bosh/website-release/jobs/http/templates/etc/letsencrypt.config.sh
@@ -3,3 +3,5 @@
 export WELLKNOWN="/var/vcap/data/http/letsencrypt"
 export PRIVATE_KEY="/var/vcap/jobs/http/etc/letsencrypt.key"
 export CONTACT_EMAIL="<%= p('http.letsencrypt.email') %>"
+
+mkdir -p "$WELLKNOWN"
---
+++ ci/tasks/renew-letsencrypt/run.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+set -e
+set -u
+
+cd updated-infra
+
+git clone file://$PWD/../infra .
+
+if [ -n "$target_ca_crt" ] ; then
+  echo "$target_ca_crt" > /tmp/ca.crt
+  target_args="--ca-cert /tmp/ca.crt"
+fi
+
+echo "$target bosh" >> /etc/hosts
+
+gobosh target "https://bosh:25555" ${target_args:-}
+gobosh --user "$username" --password "$password" log-in
+gobosh deployment "$deployment"
+
+gobosh ssh -c 'sudo /var/vcap/jobs/http/bin/letsencrypt --cron' | tee /tmp/cron
+
+if grep -q ' + Valid till' /tmp/cron ; then
+  exit
+fi
+
+domain=$( grep '^Processing ' /tmp/cron | awk '{ print $2 }' )
+
+echo 'Downloading new certificate'
+
+gobosh ssh -r -c 'cat /var/vcap/jobs/http/etc/tls.crt' | cut -f2 > "$tls_crt"
+
+echo 'Downloading new key'
+
+gobosh ssh -r -c 'sudo cat /var/vcap/jobs/http/etc/tls.key' | cut -f2 > "$tls_key"
+
+echo 'Committing'
+
+git config user.email "$git_user_email"
+git config user.name "$git_user_name"
+
+git commit -m "Renew letsencrypt for $domain" -- "$tls_crt" "$tls_key"
+
+echo 'Done'
