47a9051e5c9ee55c2e9247dea96bd6b8bb683478
Danny Berger <dpb587@gmail.com>
2016-12-18 09:44:04 -0700

    Refactor renew-letsencrypt

--- ci/tasks/renew-letsencrypt/run.sh
+++ ci/tasks/renew-letsencrypt/run.sh
@@ -1,44 +1,58 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
-set -u
+set -eu -o pipefail
 
-cd updated-infra
+REM() { /bin/echo $( date -u +"%Y-%m-%dT%H:%M:%SZ" ) "$@"; }
 
-git clone file://$PWD/../infra .
+REM 'starting'
 
-if [ -n "$target_ca_crt" ] ; then
-  echo "$target_ca_crt" > /tmp/ca.crt
-  target_args="--ca-cert /tmp/ca.crt"
-fi
+REM 'renewing certificate'
 
-echo "$target bosh" >> /etc/hosts
-
-gobosh target "https://bosh:25555" ${target_args:-}
-gobosh --user "$username" --password "$password" log-in
-gobosh deployment "$deployment"
-
-gobosh ssh -r -c 'sudo /var/vcap/jobs/http/bin/letsencrypt --cron' | cut -f2 | tee /tmp/cron
+bosh ssh -r -c 'sudo /var/vcap/jobs/http/bin/letsencrypt --cron' \
+  | cut -f2 \
+  | tee /tmp/cron
 
 if grep -q ' + Valid till' /tmp/cron ; then
+  REM 'finished'
+
   exit
 fi
 
-domain=$( grep 'Processing ' /tmp/cron | rev | cut -d ' ' -f 1 | rev )
+REM 'downloading certificate'
 
-echo 'Downloading new certificate'
+bosh ssh -r -c 'cat /var/vcap/jobs/http/etc/tls.crt' \
+  | cut -f2 > /tmp/tls.crt
 
-gobosh ssh -r -c 'cat /var/vcap/jobs/http/etc/tls.crt' | cut -f2 > "$tls_crt"
+REM 'downloading private key'
 
-echo 'Downloading new key'
+bosh ssh -r -c 'sudo cat /var/vcap/jobs/http/etc/tls.key' \
+  | cut -f2 > /tmp/tls.key
 
-gobosh ssh -r -c 'sudo cat /var/vcap/jobs/http/etc/tls.key' | cut -f2 > "$tls_key"
+REM 'patching deployment'
 
-echo 'Committing'
+jq -cn \
+  --arg path "$secrets_path" \
+  --arg certificate "$( cat /tmp/tls.crt )" \
+  --arg private_key "$( cat /tmp/tls.key )" \
+  '
+    [
+      {
+        "type": "replace",
+        "path": "\($path)/certificate",
+        "value": $certificate
+      },
+      {
+        "type": "replace",
+        "path": "\($path)/private_key",
+        "value": $private_key
+      }
+    ]
+  ' \
+  | bosh interpolate \
+    --ops-file /dev/stdin \
+    $secrets_file \
+    > $secrets_file.new
 
-git config user.email "$git_user_email"
-git config user.name "$git_user_name"
+mv $secrets_file.new $secrets_file
 
-git commit -m "Renew letsencrypt for $domain" -- "$tls_crt" "$tls_key"
-
-echo 'Done'
+REM 'finished'
