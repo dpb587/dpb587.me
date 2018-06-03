a7c331f2fc41df5ee4ab561622f7c03c56249358
Danny Berger <dpb587@gmail.com>
2018-06-03 05:23:30 -0700

    remove ssl/letsencrypt responsibilities from nginx job

--- bosh/website-release/jobs/http/templates/etc/letsencrypt.config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export WELLKNOWN="/var/vcap/data/http/letsencrypt"
-export PRIVATE_KEY="/var/vcap/jobs/http/etc/letsencrypt.key"
-export CONTACT_EMAIL="<%= p('http.letsencrypt.email') %>"
-
-mkdir -p "$WELLKNOWN"
