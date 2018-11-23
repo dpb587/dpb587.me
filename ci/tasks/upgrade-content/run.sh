c4a30dde64962dc4a7c168a023c05fe9b13b4451
Danny Berger <dpb587@gmail.com>
2018-11-23 07:01:03 -0800

    test deploy after upgrades; add content watch

---
+++ ci/tasks/upgrade-content/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -eu
+
+export GIT_COMMITTER_NAME="Concourse"
+export GIT_COMMITTER_EMAIL="concourse.ci@localhost"
+
+git config --global user.email "${git_user_email:-ci@localhost}"
+git config --global user.name "${git_user_name:-CI Bot}"
+
+git clone --recurse-submodules file://$PWD/repo-input repo
+
+cd content-repo
+
+content_commit=$( git rev-parse HEAD )
+
+cd ../repo/src/content
+
+git checkout $content_commit
+
+cd ../..
+
+git add src/content
+
+if git diff --staged --exit-code --quiet ; then
+  # no changes pending
+  exit
+fi
+
+git commit -m "src: Update content to $( echo "$content_commit" | cut -c-7 )"
