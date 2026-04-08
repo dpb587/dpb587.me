d9a6711a8579acb472a0b90dd48f221bb46d642a
Danny Berger <dpb587@gmail.com>
2026-04-08 04:11:48 +0000

    theme: trim trailing permalink slash; alternate-links block

--- scripts/package-public.sh
+++ scripts/package-public.sh
@@ -6,13 +6,19 @@ set -euxo pipefail
 
 target="$( cd "${1}" && pwd )"
 
-mkdir -p tmp
-
-cd tools
-
-go run ./publish/cmd/noslash/main.go "${target}" >../tmp/noslash.log 2>&1
-
 cd "${target}"
+
+if grep -Esnr 'x0pqdrbrnr4h(/|%2F)' .
+then
+    echo "error: found non-canonical page link"
+
+    exit 1
+elif grep -Esnr "black-canyon-of-the-gunnison-park(/|%2F)(<|'|\")" .
+then
+    echo "error: found non-canonical page link"
+
+    exit 1
+fi
 
 # sync with server/main.go
 find . -type f \( -name '*.css' -o -name '*.html' -o -name '*.js' -o -name '*.svg' -o -name '*.xml' \) \
